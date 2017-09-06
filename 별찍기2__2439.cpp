6406c23af37e0a087fa1f29a431e1db99abd9b4c
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-07 00:32:17 +0900

    commit

--- 별찍기2__2439.cpp
+++ 별찍기2__2439.cpp
@@ -1,22 +1,22 @@
-////
-////  별찍기2__2439.cpp
-////  Algorithm_study_group
-////
-////  Created by LSH on 2017. 9. 5..
-////  Copyright © 2017년 LSH. All rights reserved.
-////
 //
-//#include <stdio.h>
+//  별찍기2__2439.cpp
+//  Algorithm_study_group
 //
-//int main(){
-//    int N;
-//    scanf("%d",&N);
-//    for(int i = 1; i <= N; ++i){
-//        for(int j = 1; j < i; ++j)
-//            printf(" ");
-//        for(int j = N - i; j >= 0; --j)
-//            printf("*");
-//        printf("\n");
-//    }
-//    return 0;
-//}
+//  Created by LSH on 2017. 9. 5..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    for(int i = 1; i <= N; ++i){
+        for(int j = 1; j < i; ++j)
+            printf(" ");
+        for(int j = N - i; j >= 0; --j)
+            printf("*");
+        printf("\n");
+    }
+    return 0;
+}
--- 유기농배추__1012.cpp
+++ 유기농배추__1012.cpp
@@ -1,52 +1,52 @@
-////
-////  유기농배추.cpp
-////  Algorithm_study_group
-////
-////  Created by LSH on 2017. 7. 29..
-////  Copyright © 2017년 LSH. All rights reserved.
-////
 //
-//#include <cstdio>
-//#include <cstring>
+//  유기농배추.cpp
+//  Algorithm_study_group
 //
-//using namespace std;
-//const int MAX = 50;
-//bool field[MAX][MAX];
-//int M, N, K;
-//int x, y;
-//void dfs(int, int);
+//  Created by LSH on 2017. 7. 29..
+//  Copyright © 2017년 LSH. All rights reserved.
 //
-//int main(){
-//    int T;
-//    scanf("%d", &T);
-//    while(T--){
-//        int count = 0;
-//        scanf("%d %d %d", &M, &N, &K);
-//        for(int i = 0; i < K; ++i){
-//            scanf("%d %d", &x, &y);
-//            field[x][y] = 1;
-//        }
-//        for(int i = 0; i < M; ++i){
-//            for(int j = 0; j <N; ++j){
-//                if(field[i][j] == 1){
-//                    dfs(i,j);
-//                    count++;
-//                }
-//            }
-//        } printf("%d\n", count);
-//    } return 0;
-//}
-//
-//void dfs(int x, int y){
-//    field[x][y] = 0;
-//    if(x < 1 || x >= M-1 || y < 1 || y >= N-1)
-//        return ;
-//    if(field[x-1][y] == 1)
-//        dfs(x-1, y);
-//    if(field[x+1][y] == 1)
-//        dfs(x+1, y);
-//    if(field[x][y-1] == 1)
-//        dfs(x, y-1);
-//    if(field[x][y+1] == 1)
-//        dfs(x, y+1);
-//}
+
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+const int MAX = 50;
+bool field[MAX][MAX];
+int M, N, K;
+int x, y;
+void dfs(int, int);
+
+int main(){
+    int T;
+    scanf("%d", &T);
+    while(T--){
+        int count = 0;
+        scanf("%d %d %d", &M, &N, &K);
+        for(int i = 0; i < K; ++i){
+            scanf("%d %d", &x, &y);
+            field[x][y] = 1;
+        }
+        for(int i = 0; i < M; ++i){
+            for(int j = 0; j <N; ++j){
+                if(field[i][j] == 1){
+                    dfs(i,j);
+                    count++;
+                }
+            }
+        } printf("%d\n", count);
+    } return 0;
+}
+
+void dfs(int x, int y){
+    field[x][y] = 0;
+    if(x < 1 || x >= M-1 || y < 1 || y >= N-1)
+        return ;
+    if(field[x-1][y] == 1)
+        dfs(x-1, y);
+    if(field[x+1][y] == 1)
+        dfs(x+1, y);
+    if(field[x][y-1] == 1)
+        dfs(x, y-1);
+    if(field[x][y+1] == 1)
+        dfs(x, y+1);
+}
