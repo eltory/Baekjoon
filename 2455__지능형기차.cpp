9226c921ca04b0532d1b81550359585abb2f7017
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-09 21:14:17 +0900

    commit

--- 2455__지능형기차.cpp
+++ 2455__지능형기차.cpp
@@ -7,3 +7,20 @@
 //
 
 #include <stdio.h>
+int people[4][2] = {0};
+int main(){
+    int max = 0, total = 0;
+    int in, out;
+    for(int i = 0; i < 4; ++i){
+        scanf("%d %d", &out, &in);
+        if(i == 0){
+            total = in - out;
+            max = total;
+        }else{
+            total += (in - out);
+            if(max < total)
+                max = total;
+        }
+    } printf("%d", max);
+    return 0;
+}
