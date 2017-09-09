b86d5aed6a027fc921075bc8e4bf3818904512d3
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-09 21:12:21 +0900

    commit

---
+++ 1094__막대기.cpp
@@ -0,0 +1,23 @@
+////
+////  1094.cpp
+////  Algorithm_study_group
+////
+////  Created by LSH on 2017. 9. 9..
+////  Copyright © 2017년 LSH. All rights reserved.
+////
+//
+//#include <stdio.h>
+//int main(){
+//    int X, num = 0;
+//    scanf("%d", &X);
+//    for(int i = 64; i > 0; i/=2){
+//        if(!X)
+//            break;
+//        if(i <= X){
+//            num++;
+//            X -= i;
+//        }
+//    }
+//    printf("%d", num);
+//    return 0;
+//}
---
+++ 2455__지능형기차.cpp
@@ -0,0 +1,9 @@
+//
+//  2455__지능형기차.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 9. 9..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
