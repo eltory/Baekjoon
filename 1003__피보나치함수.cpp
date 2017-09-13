e5a9bb24d69ef66bccfb7e88d94796520eac21d9
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-13 15:42:53 +0900

    commit

--- 1003__피보나치함수.cpp
+++ 1003__피보나치함수.cpp
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+int dp[40][2] = {};
+int N;
+void fibo(int );
+int main(){
+    scanf("%d", &N);
+    while(N--){
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        int n;
+        scanf("%d", &n);
+        fibo(n);
+        printf("%d %d\n", dp[n][0], dp[n][1]);
+    } return 0;
+}
+
+void fibo(int n){
+    for(int i = 2; i <= n; ++i){
+        dp[i][0] = dp[i-2][0] + dp[i-1][0];
+        dp[i][1] = dp[i-2][1] + dp[i-1][1];
+    }
+}
