17ccf3a5b2368c3aa315ebdf836cd686dd046be3
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-19 11:33:15 +0900

    commit

---
+++ 11654__아스키코드.cpp
@@ -0,0 +1,15 @@
+//
+//  11654__아스키코드.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 9. 18..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    char input;
+    scanf("%c", &input);
+    printf("%d",input);
+    return 0;
+}
