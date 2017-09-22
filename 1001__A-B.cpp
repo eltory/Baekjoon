c29af26393c23ef308be4f7292cb0392124f88df
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-22 21:47:19 +0900

    commit

---
+++ 1001__A-B.cpp
@@ -0,0 +1,15 @@
+//
+//  1001__A-B.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 9. 22..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    int a, b;
+    scanf("%d %d",&a, &b);
+    printf("%d",a-b);
+    return 0;
+}
