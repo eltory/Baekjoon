82ccf3d1897da5f470760289b2501766af0ef106
Sung Hwi Lee <eltoryn7@gmail.com>
2017-09-27 09:46:17 +0900

    commit

---
+++ 1008__AdivB.cpp
@@ -0,0 +1,15 @@
+//
+//  1008__A/B.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 9. 27..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
+int main(){
+    double a, b;
+    scanf("%lf %lf",&a, &b);
+    printf("%.9lf\n",a/b);
+    return 0;
+}
