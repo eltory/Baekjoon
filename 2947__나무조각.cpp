aeec771a11da87c647f758c0a01eeb07caea3276
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-16 11:55:16 +0900

    commit

--- 2947__나무조각.cpp
+++ 2947__나무조각.cpp
@@ -7,3 +7,22 @@
 //
 
 #include <stdio.h>
+
+int main(){
+    int wood[5];
+    for(int i = 0; i < 5; ++i)
+        scanf("%d", &wood[i]);
+    for(int i = 0; i < 4; ++i)
+        for(int j = 0; j < 5; ++j){
+            if(wood[j] > wood[j+1]){
+                int tmp = wood[j+1];
+                wood[j+1] = wood[j];
+                wood[j] = tmp;
+                for(int k = 0; k < 5; ++k)
+                    printf("%d ", wood[k]);
+                printf("\n");
+            }
+        }
+    return 0;
+}
+
