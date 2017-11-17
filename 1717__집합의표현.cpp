5c638384540bec2a6ece7c514803c262ef90b15a
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-17 10:37:07 +0900

    commit

--- 1717__집합의표현.cpp
+++ 1717__집합의표현.cpp
@@ -7,3 +7,41 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+int N[1000001];
+
+int findSet(int);
+void mergeSet(int, int);
+
+int main(){
+    int n, m;
+    scanf("%d %d", &n, &m);
+    memset(N, -1, sizeof(N));
+    for(int i = 0; i < m; ++i){
+        int a, b, c;
+        scanf("%d %d %d", &c, &a, &b);
+        if(c == 0)
+            mergeSet(a, b);
+        else{
+            if(findSet(a) == findSet(b))
+                printf("YES\n");
+            else
+                printf("NO\n");
+        }
+    }
+    return 0;
+}
+
+int findSet(int a){
+    if(N[a] < 0)
+        return a;
+    return N[a] = findSet(N[a]);
+}
+void mergeSet(int a, int b){
+    a = findSet(a);
+    b = findSet(b);
+    if(a == b)
+        return;
+    N[b] = a;
+}
+
