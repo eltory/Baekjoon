f50834f22fe58e0aa081dba7f935109640102281
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-18 19:54:07 +0900

    commit

--- 2606__바이러스.cpp
+++ 2606__바이러스.cpp
@@ -7,3 +7,35 @@
 //
 
 #include <stdio.h>
+#include <algorithm>
+using namespace std;
+const int INF = 1e8;
+const int MAX_N = 100;
+
+int computer[MAX_N][MAX_N];
+int total = 0;
+
+int main(){
+    int n, m, a, b;
+    scanf("%d", &n);
+    scanf("%d", &m);
+    for(int i = 0; i < n; ++i){
+        fill(computer[i], computer[i] + n, INF);
+        computer[i][i] = 0;
+    }
+    for(int i = 0; i < m; ++i){
+        scanf("%d %d", &a, &b);
+        computer[a-1][b-1] = 1;
+        computer[b-1][a-1] = 1;
+    }
+    for(int k = 0; k < n; ++k)
+        for(int i = 0; i < n; ++i)
+            for(int j = 0; j < n; ++j)
+                computer[i][j] = min(computer[i][j], computer[i][k] + computer[k][j]);
+    for(int i = 0; i < n; ++i)
+        if(computer[0][i] != INF)
+            total++;
+    printf("%d", total-1);
+    return 0;
+}
+
