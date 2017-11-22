a893b89c1c54e344790b48b691f7c6e8f1a0770e
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-23 00:34:15 +0900

    commit

--- 14920__3n+1수열.cpp
+++ 14920__3n+1수열.cpp
@@ -7,3 +7,14 @@
 //
 
 #include <stdio.h>
+
+int main(){
+    int C, n = 1;
+    scanf("%d", &C);
+    while(C != 1){
+        C = C % 2 == 0 ? C / 2 : 3 * C + 1;
+        n++;
+    } printf("%d", n);
+    return 0;
+}
+
---
+++ 14921__용액합성하기.cpp
@@ -0,0 +1,9 @@
+//
+//  14921__용액합성하기.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 11. 22..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
