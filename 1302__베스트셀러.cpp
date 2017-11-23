14543d375fdd72d9a9e9cebb5f4849083f0b0c76
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-24 00:13:11 +0900

    commit

---
+++ 1302__베스트셀러.cpp
@@ -0,0 +1,9 @@
+//
+//  1302__베스트셀러.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 11. 23..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
--- 2743__단어길이재기.cpp
+++ 2743__단어길이재기.cpp
@@ -7,3 +7,17 @@
 //
 
 #include <stdio.h>
+
+int main(){
+    char word[102];
+    int i = 0, cnt = 0;
+    gets(word);
+    while (word[i]!= '\0') {
+        if((word[i] < 91 && word[i] > 64)||(word[i] < 123 && word[i] > 96))
+            cnt++;
+        i++;
+    }
+    printf("%d", cnt);
+    return 0;
+}
+
