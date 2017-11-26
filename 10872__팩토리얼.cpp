2de0a29409180cbd3f74862a5bd72a5756aadc0b
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-26 19:06:15 +0900

    commit

--- 10872__팩토리얼.cpp
+++ 10872__팩토리얼.cpp
@@ -7,3 +7,13 @@
 //
 
 #include <stdio.h>
+int main(){
+    int N, factorial = 1;
+    scanf("%d", &N);
+    for(int i = 1; i <= N; ++i)
+        factorial *= i;
+    printf("%d", factorial);
+    return 0;
+}
+
+
