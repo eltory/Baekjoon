ed11cace315b619353d5fa54419df066b73322bf
Sung Hwi Lee <eltoryn7@gmail.com>
2017-11-27 20:48:09 +0900

    commit

--- 2920__음계.cpp
+++ 2920__음계.cpp
@@ -7,3 +7,21 @@
 //
 
 #include <stdio.h>
+
+int main(){
+    int piano[9], order[3];
+    for(int i = 1; i <= 8; ++i)
+        scanf("%d", &piano[i]);
+    order[0] = piano[1] == 1 ? 1 : 0;
+    order[1] = piano[1] == 8 ? 1 : 0;
+    order[2] = (order[0] || order[1]) == 0 ? 1 : 0;
+    for(int i = 1; i <= 8; ++i)
+        if((order[0] && (piano[i] != i)) || (order[1] && (piano[i] != 9 - i)))
+            order[2] = 1;
+    if(order[2])
+        printf("mixed");
+    else
+        printf("%s", order[0] == 1 ? "ascending" : "descending");
+    return 0;
+}
+
