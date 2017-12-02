5f199abf35d180952a1a8c272b33abaf015449b0
Sung Hwi Lee <eltoryn7@gmail.com>
2017-12-02 18:00:04 +0900

    commit

---
+++ 10973__이전순열.cpp
@@ -0,0 +1,9 @@
+//
+//  10973__이전순열.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 12. 1..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ 1722__순열의순서.cpp
@@ -0,0 +1,9 @@
+//
+//  1722__순열의순서.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 12. 1..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
