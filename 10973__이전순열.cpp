e9cf57ce75c2caf50036c19b315d4044d9aa0c1f
Sung Hwi Lee <eltoryn7@gmail.com>
2017-12-04 13:53:31 +0900

    commit

--- 10973__이전순열.cpp
+++ 10973__이전순열.cpp
@@ -7,3 +7,26 @@
 //
 
 #include <stdio.h>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N, p;
+    vector<int> permutation;
+    scanf("%d", &N);
+    
+    for(int i = 0; i < N; ++i){
+        scanf("%d", &p);
+        permutation.push_back(p);
+    }
+    if(prev_permutation(permutation.begin(), permutation.end())){
+        for(int i = 0; i < N; ++i)
+            printf("%d ", permutation[i]);
+        printf("\n");
+    }else
+        printf("-1\n");
+    return 0;
+}
+
+
