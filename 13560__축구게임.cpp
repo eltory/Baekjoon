cf318ad5598796e40cb099f5c854bce81b638c7c
Sung Hwi Lee <eltoryn7@gmail.com>
2017-12-07 14:24:47 +0900

    commit

---
+++ 13560__축구게임.cpp
@@ -0,0 +1,9 @@
+//
+//  13560__축구게임.cpp
+//  Algorithm_study_group
+//
+//  Created by LSH on 2017. 12. 7..
+//  Copyright © 2017년 LSH. All rights reserved.
+//
+
+#include <stdio.h>
--- 1927__최소힙.cpp
+++ 1927__최소힙.cpp
@@ -7,3 +7,24 @@
 //
 
 #include <stdio.h>
+#include <queue>
+using namespace std;
+priority_queue<int> pq;
+int main(){
+    int N, x;
+    scanf("%d",&N);
+    for(int i = 0; i < N; ++i){
+        scanf("%d", &x);
+        if(x == 0){
+            if(!pq.empty()){
+                printf("%d\n", pq.top());
+                pq.pop();
+            }else
+                printf("0\n");
+        }else
+             pq.push(x);
+    } return 0;
+}
+
+
+
