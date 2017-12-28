0a0fd00023dbf21883e1112b180277947985f3d5
Sung Hwi Lee <eltoryn7@gmail.com>
2017-12-29 01:12:31 +0900

    commit

---
+++ 2167__2차원배열의합.cpp
@@ -0,0 +1,31 @@
+////
+////  2167__2차원배열의합.cpp
+////  Algorithm_study_group
+////
+////  Created by LSH on 2017. 12. 28..
+////  Copyright © 2017년 LSH. All rights reserved.
+////
+//
+//#include <stdio.h>
+//const int MAX = 301;
+//int arr[MAX][MAX];
+//int dp[MAX][MAX];
+//
+//int main(){
+//    int N, M, K;
+//    scanf("%d %d", &N, &M);
+//    for(int i = 1; i <= N; ++i)
+//        for(int j = 1; j <= M; ++j)
+//            scanf("%d", &arr[i][j]);
+//    scanf("%d", &K);
+//    for(int k = 0; k < K; ++k){
+//        int i, j, x, y, sum = 0;
+//        scanf("%d %d %d %d", &i, &j, &x, &y);
+//        for(int a = i; a <= x; ++a){
+//            for(int b = j; b <= y; ++b){
+//                sum += arr[a][b];
+//            }
+//        } printf("%d\n",sum);
+//    } return 0;
+//}
+
