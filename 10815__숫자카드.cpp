c478814d8fce6e6efe6e60a6ce7324f7f84480c9
Sung Hwi Lee <eltoryn7@gmail.com>
2018-01-25 21:53:33 +0900

    commit

--- 10815__숫자카드.cpp
+++ 10815__숫자카드.cpp
@@ -6,4 +6,26 @@
 //  Copyright © 2018년 LSH. All rights reserved.
 //
 
+/**
+ *  단순 이분탐색
+ */
 #include <stdio.h>
+#include <vector>
+#include <algorithm>
+using namespace std;
+vector<int> card;
+int main(){
+    int N, M, num;
+    scanf("%d", &N);
+    card.resize(N);
+    for(int i = 0; i < N; ++i)
+        scanf("%d", &card[i]);
+    sort(card.begin(), card.end());
+    scanf("%d", &M);
+    for(int i = 0; i < M; ++i){
+        scanf("%d", &num);
+        printf("%d ", binary_search(card.begin(), card.end(), num) ? 1 : 0);
+    }
+    return 0;
+}
+
